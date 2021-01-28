6f455d145930e683d7b08bfb32530e3976b209da
Dan Wilder <danwilder@Dans-MacBook-Pro.local>
2021-01-28 15:14:00 -0600

    Complete 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // Hardcoded values for now
 let persons = [
   {
@@ -53,13 +55,42 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
 
-  console.log(persons, `\nid to be deleted ${id}`);
+  console.log("DELETE", id);
 
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
 
-  console.log(persons);
+const generateId = () => {
+  const range = 1000000;
+  return Math.floor(Math.random() * range);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log("POST", body);
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
