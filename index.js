e59502c650560613b671f064c4b541edda4b7093
Dan Wilder <danwilder@Dans-MacBook-Pro.local>
2021-01-28 14:41:38 -0600

    Complete 3.4

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  console.log(persons, `\nid to be deleted ${id}`);
+
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+
+  console.log(persons);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
