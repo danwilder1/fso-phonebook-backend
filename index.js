9033edb34c98bebca379c1ccde8fd5bbc10eee75
Dan Wilder <danwilder@Dans-MacBook-Pro.local>
2021-01-29 09:18:14 -0600

    Complete 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 var morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("post-data", (req, res) => {
